9955acf850396b720e94bc4423f941d2ed4b32a4
minsoo.lee <letomok@gmail.com>
2022-11-30 01:26:18 +0900

    Particle

--- src/chapter6/mod.rs
+++ src/chapter6/mod.rs
@@ -5,3 +5,4 @@ pub mod ex6_4;
 pub mod ex6_5;
 pub mod ex6_6;
 pub mod ex6_7;
+pub mod ex6_9;
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod chapter6;
 use chapter6::*;
 
 fn main() {
-    ex6_7::example()
+    ex6_9::example()
 }
